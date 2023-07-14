ff9fa35cc1bde00a57add7651fbace5307e05e04
narendra <51484021+alenway@users.noreply.github.com>
2023-07-14 12:22:33 +0530

    basic file structure

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -4,7 +4,14 @@ import { About, Footer, Header, Skills, Testimonial, Work } from './container';
 
 const App = () => {
   return(
-    <div>App
+    <div className="app">
+    <Header/>
+    <h1>sidflis</h1>
+    <About/>
+    <Work/>
+    <Skills/>
+    <Testimonial/>
+    <Footer/>
     </div>
   )
   };
